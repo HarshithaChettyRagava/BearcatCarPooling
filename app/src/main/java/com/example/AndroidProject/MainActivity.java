ec512109cab58de08e6885830873b4111a2caf8a
s533491 <s533491@NWMISSOURI.EDU>
2019-03-11 20:10:01 -0500

    Commit the Sign up and Sign in pages

--- app/src/main/java/com/example/AndroidProject/MainActivity.java
+++ app/src/main/java/com/example/AndroidProject/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +15,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void Button_SignUp(View v){
-        Intent in = new Intent(this,SignUpPage.class);
+
+    public void Button_SignUp (View v){
+        Intent in = new Intent(this, SignUpPage.class);
         startActivity(in);
     }
 
+
+
     public void Button_SignIn(View v){
-        Intent in = new Intent(this,SignUpPage.class);
-        startActivity(in);
+        Intent sn = new Intent(this,SignInPage.class);
+        startActivity(sn);
     }
 }
