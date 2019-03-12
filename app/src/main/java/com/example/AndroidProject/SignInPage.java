ce4e207b2d5bcb21f5ae2db4537b291df0fc7883
vamshi nuka <Vamshinuka@live.in>
2019-03-11 20:58:52 -0500

    added booking page

--- app/src/main/java/com/example/AndroidProject/SignInPage.java
+++ app/src/main/java/com/example/AndroidProject/SignInPage.java
@@ -1,7 +1,9 @@
 package com.example.AndroidProject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignInPage extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class SignInPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_page);
     }
+
+    public void buttonLogin(View v){
+        Intent inn = new Intent(this,CarBooking.class);
+        startActivity(inn);
+    }
+
+
 }
