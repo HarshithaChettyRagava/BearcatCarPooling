4339e649be590553d8c88454b9a10b812d9dd794
vamshi nuka <Vamshinuka@live.in>
2019-04-07 00:25:17 -0500

    Updated SignInPage.java and added CONTRIBUTION_Nuka.md with the list of my contributions.

---
+++ CONTRIBUTION_Nuka.md
@@ -0,0 +1,17 @@
+# Project Name
+BEARCAT CAR POOLING
+
+# Project Team Members
+- Harshitha Chetty Ragava
+- Gouthami Pasham
+- Vamshi Krishna Nuka
+
+# My Contributions towards project
+- UI Design
+- Booking Page
+- Confirmation Page
+- ER Diagram
+- Issues with my local datbase on my pc
+- Validation on mandatory fields using asterick
+- Signup page
+- signin page 
--- app/src/main/java/com/example/AndroidProject/SignInPage.java
+++ app/src/main/java/com/example/AndroidProject/SignInPage.java
@@ -49,26 +49,6 @@ public class SignInPage extends AppCompatActivity {
 
 
     public void buttonLogin(View v){
-//        db = new MyHelper(this);
-////        Cursor cursor = db.allData();
-////        if(cursor.getCount()==0){
-////            Toast.makeText(getApplicationContext(),"No rows found"+cursor.getCount(),Toast.LENGTH_SHORT).show();
-////        }
-////        else{
-////            Toast.makeText(getApplicationContext(),"No of rows found"+cursor.getCount(),Toast.LENGTH_SHORT).show();
-////            Log.d("No of rows are"," "+cursor.getCount());
-////            //cursor.moveToFirst();
-////            while(cursor.moveToNext()){
-////                Toast.makeText(getApplicationContext(),"UserName is :"+cursor.getString(0),Toast.LENGTH_SHORT).show();
-////                Toast.makeText(getApplicationContext(),"Password is :"+cursor.getString(1),Toast.LENGTH_SHORT).show();
-////                Toast.makeText(getApplicationContext(),"Address is :"+cursor.getString(2),Toast.LENGTH_SHORT).show();
-////                Toast.makeText(getApplicationContext(),"Phone No is :"+cursor.getString(3),Toast.LENGTH_SHORT).show();
-////                Toast.makeText(getApplicationContext(),"Password is :"+cursor.getString(4),Toast.LENGTH_SHORT).show();
-////                Toast.makeText(getApplicationContext(),"ConfirmPwd is :"+cursor.getString(5),Toast.LENGTH_SHORT).show();
-////                Username = cursor.getString(0);
-////                Password = cursor.getString(1);
-//            }
-//        }
         String userNameIn = EmailId.getText().toString();
         String userPwdIn = UserPwd.getText().toString();
 
