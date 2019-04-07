5fefb7d885ff4b70107a37f369e00af75170eb16
vamshi nuka <Vamshinuka@live.in>
2019-04-07 00:11:24 -0500

    Updated SignInPage.java with successfully logging with username and password an also validated negative scenario where the credentials are bad.

--- app/src/main/java/com/example/AndroidProject/SignInPage.java
+++ app/src/main/java/com/example/AndroidProject/SignInPage.java
@@ -1,18 +1,21 @@
 package com.example.AndroidProject;
 
 import android.content.Intent;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
-
+import com.parse.FindCallback;
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseInstallation;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 import java.util.List;
 
 public class SignInPage extends AppCompatActivity {
@@ -29,30 +32,82 @@ public class SignInPage extends AppCompatActivity {
         EmailId = (EditText) findViewById(R.id.emailIdET);
         UserPwd = (EditText) findViewById(R.id.passwordET);
 
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                // if defined
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build()
+        );
+
+        //ParseUser.enableRevocableSessionInBackground();
+        //ParseUser.getCurrentUser().logOut();
+
+        // Save the current Installation to Back4App
+        ParseInstallation.getCurrentInstallation().saveInBackground();
     }
 
 
     public void buttonLogin(View v){
-        db = new MyHelper(this);
-        Cursor cursor = db.allData();
-        if(cursor.getCount()==0){
-            Toast.makeText(getApplicationContext(),"No rows found"+cursor.getCount(),Toast.LENGTH_SHORT).show();
-        }
-        else{
-            Toast.makeText(getApplicationContext(),"No of rows found"+cursor.getCount(),Toast.LENGTH_SHORT).show();
-            Log.d("No of rows are"," "+cursor.getCount());
-            //cursor.moveToFirst();
-            while(cursor.moveToNext()){
-                Toast.makeText(getApplicationContext(),"UserName is :"+cursor.getString(0),Toast.LENGTH_SHORT).show();
-                Toast.makeText(getApplicationContext(),"Password is :"+cursor.getString(1),Toast.LENGTH_SHORT).show();
-                Toast.makeText(getApplicationContext(),"Address is :"+cursor.getString(2),Toast.LENGTH_SHORT).show();
-                Toast.makeText(getApplicationContext(),"Phone No is :"+cursor.getString(3),Toast.LENGTH_SHORT).show();
-                Toast.makeText(getApplicationContext(),"Password is :"+cursor.getString(4),Toast.LENGTH_SHORT).show();
-                Toast.makeText(getApplicationContext(),"ConfirmPwd is :"+cursor.getString(5),Toast.LENGTH_SHORT).show();
-                Username = cursor.getString(0);
-                Password = cursor.getString(1);
+//        db = new MyHelper(this);
+////        Cursor cursor = db.allData();
+////        if(cursor.getCount()==0){
+////            Toast.makeText(getApplicationContext(),"No rows found"+cursor.getCount(),Toast.LENGTH_SHORT).show();
+////        }
+////        else{
+////            Toast.makeText(getApplicationContext(),"No of rows found"+cursor.getCount(),Toast.LENGTH_SHORT).show();
+////            Log.d("No of rows are"," "+cursor.getCount());
+////            //cursor.moveToFirst();
+////            while(cursor.moveToNext()){
+////                Toast.makeText(getApplicationContext(),"UserName is :"+cursor.getString(0),Toast.LENGTH_SHORT).show();
+////                Toast.makeText(getApplicationContext(),"Password is :"+cursor.getString(1),Toast.LENGTH_SHORT).show();
+////                Toast.makeText(getApplicationContext(),"Address is :"+cursor.getString(2),Toast.LENGTH_SHORT).show();
+////                Toast.makeText(getApplicationContext(),"Phone No is :"+cursor.getString(3),Toast.LENGTH_SHORT).show();
+////                Toast.makeText(getApplicationContext(),"Password is :"+cursor.getString(4),Toast.LENGTH_SHORT).show();
+////                Toast.makeText(getApplicationContext(),"ConfirmPwd is :"+cursor.getString(5),Toast.LENGTH_SHORT).show();
+////                Username = cursor.getString(0);
+////                Password = cursor.getString(1);
+//            }
+//        }
+        String userNameIn = EmailId.getText().toString();
+        String userPwdIn = UserPwd.getText().toString();
+
+
+//        ParseQuery<ParseObject> allQuery = ParseQuery.getQuery("User");
+////        allQuery.whereEqualTo("username",userNameIn);
+////        allQuery.whereEqualTo("password",userPwdIn);
+////        allQuery.findInBackground(new FindCallback<ParseObject>() {
+////            @Override
+////            public void done(List<ParseObject> objects, ParseException e) {
+////
+////            }
+////        });
+
+        ParseUser.logInInBackground(userNameIn,userPwdIn, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                Log.d("Parse","Login Result: "+e);
+                if(e!=null){
+                    Toast.makeText(getApplicationContext(),"Error: "+e,Toast.LENGTH_LONG).show();
+                    Log.d("Parse","Error: "+e.getMessage());
+                    EmailId.setText("");
+                    UserPwd.setText("");
+                }
+                else{
+                    Log.d("Parse","Successfully logged in");
+                    EmailId.setText("");
+                    UserPwd.setText("");
+                }
             }
-        }
+        });
+
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                SignInPage.this.finish();
+//            }
+//        }, 10000);
+
         Intent inn = new Intent(this,CarBooking.class);
         startActivity(inn);
     }
